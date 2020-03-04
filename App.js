fb2223133536a7efe4f808e2f195485b0f4435a0
uit2712 <phamquangvi456@gmail.com>
2020-03-04 20:17:33 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+} from 'react-native';
+
+function App() {
+    return (
+        <Text>Hello world!</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default App;

64e60bdd3ca6a4b1d5ec787b00e888f8e79a55eb
uit2712 <phamquangvi456@gmail.com>
2020-03-07 12:39:35 +0700

    install fbsdk

--- App.js
+++ App.js
@@ -6,20 +6,69 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
-    Text,
+    Button,
+    View,
 } from 'react-native';
+import { AccessToken, LoginManager, LoginResult } from 'react-native-fbsdk';
 
 function App() {
     return (
-        <Text>Hello world!</Text>
+        <View style={styles.container}>
+            <LoginButton/>
+        </View>
     );
 };
 
+function LoginButton() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    function login() {
+        LoginManager.logInWithPermissions(['public_profile']).then((result: LoginResult) => {
+            if (result.error) {
+                console.log('Error: ', result.error);
+            } else {
+                if (result.isCancelled) {
+                    console.log('Login is cancelled');
+                } else {
+                    setLoggedIn(true);
+                    console.log('Logged in');
+                }
+            }
+        })
+    }
+
+    function logout() {
+        LoginManager.logOut();
+        setLoggedIn(false);
+        console.log('Logout');
+    }
+
+    if (loggedIn)
+        return (
+            <Button
+                title='Logout Facebook'
+                onPress={logout}
+            />
+        )
+
+    return (
+        <Button
+            title='Login with Facebook'
+            onPress={login}
+        />
+    )
+}
+
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        padding: 10,
+    }
 });
 
 export default App;

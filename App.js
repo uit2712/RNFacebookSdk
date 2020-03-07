6506e3e11516d2a690eda3ca1ab17d2c6eb20850
uit2712 <phamquangvi456@gmail.com>
2020-03-07 23:56:19 +0700

    show profile info

--- App.js
+++ App.js
@@ -6,18 +6,29 @@
  * @flow
  */
 
-import React, { useState } from 'react';
+import React, {
+    useState,
+    useEffect
+} from 'react';
 import {
     StyleSheet,
     Button,
     View,
+    Image,
+    Text,
 } from 'react-native';
-import { AccessToken, LoginManager, LoginResult } from 'react-native-fbsdk';
+import { 
+    AccessToken,
+    LoginManager,
+    GraphRequest,
+    GraphRequestManager,
+    Permissions
+} from 'react-native-fbsdk';
 
 function App() {
     return (
         <View style={styles.container}>
-            <LoginButton/>
+            <LoginButton />
         </View>
     );
 };
@@ -26,7 +37,7 @@ function LoginButton() {
     const [loggedIn, setLoggedIn] = useState(false);
 
     function login() {
-        LoginManager.logInWithPermissions(['public_profile']).then((result: LoginResult) => {
+        LoginManager.logInWithPermissions(['public_profile']).then((result) => {
             if (result.error) {
                 console.log('Error: ', result.error);
             } else {
@@ -34,7 +45,7 @@ function LoginButton() {
                     console.log('Login is cancelled');
                 } else {
                     setLoggedIn(true);
-                    console.log('Logged in');
+                    console.log('Logged in: ', result);
                 }
             }
         })
@@ -48,10 +59,13 @@ function LoginButton() {
 
     if (loggedIn)
         return (
-            <Button
-                title='Logout Facebook'
-                onPress={logout}
-            />
+            <>
+                <FbBasicInfo loggedIn={loggedIn} />
+                <Button
+                    title='Logout Facebook'
+                    onPress={logout}
+                />
+            </>
         )
 
     return (
@@ -62,12 +76,85 @@ function LoginButton() {
     )
 }
 
+function FbBasicInfo({
+    loggedIn
+}) {
+    const [avatar, setAvatar] = useState(null);
+    const [name, setName] = useState('');
+
+    function getBasicInfo() {
+        AccessToken.getCurrentAccessToken().then(data => {
+            const { accessToken } = data;
+            let graphRequest = new GraphRequest('/me', {
+                accessToken,
+                parameters: {
+                    fields: {
+                        string: 'picture.type(large),name',
+                    }
+                }
+            }, (error, result) => {
+                const {
+                    picture: {
+                        data
+                    },
+                    name,
+                } = result;
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(result);
+                    setAvatar(data);
+                    setName(name);
+                }
+            });
+    
+            const graphRequestManager = new GraphRequestManager();
+            graphRequestManager.addRequest(graphRequest).start();
+        });
+    }
+
+    useEffect(() => {
+        getBasicInfo();
+    }, [loggedIn]);
+
+    useEffect(() => {
+        console.log(avatar);
+    }, [avatar]);
+
+    if (!loggedIn || !avatar)
+        return null;
+
+    const { url, width, height } = avatar;
+    return (
+        <View style={{
+            alignItems: 'center',
+            alignContent: 'center',
+            justifyContent: 'center',
+        }}>
+            <Image
+                source={{ uri: url }}
+                style={styles.avatar}
+                width={width}
+                height={height}
+            />
+            <Text style={styles.name}>{name}</Text>
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignContent: 'center',
         padding: 10,
+    },
+    avatar: {
+        alignSelf: 'center',
+        marginBottom: 10,
+    },
+    name: {
+        marginBottom: 10,
     }
 });
 

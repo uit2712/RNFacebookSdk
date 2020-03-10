176c223c493419a926715e35ee7148b24c40d9e9
uit2712 <phamquangvi456@gmail.com>
2020-03-10 13:54:15 +0700

    temp

--- App.js
+++ App.js
@@ -16,13 +16,16 @@ import {
     View,
     Image,
     Text,
+    TextInput,
 } from 'react-native';
-import { 
+import {
     AccessToken,
     LoginManager,
     GraphRequest,
     GraphRequestManager,
-    Permissions
+    ShareLinkContent,
+    ShareDialog,
+    SharePhotoContent,
 } from 'react-native-fbsdk';
 
 function App() {
@@ -37,7 +40,7 @@ function LoginButton() {
     const [loggedIn, setLoggedIn] = useState(false);
 
     function login() {
-        LoginManager.logInWithPermissions(['public_profile']).then((result) => {
+        LoginManager.logInWithPermissions(['public_profile', 'pages_show_list', ' manage_pages']).then((result) => {
             if (result.error) {
                 console.log('Error: ', result.error);
             } else {
@@ -60,7 +63,7 @@ function LoginButton() {
     if (loggedIn)
         return (
             <>
-                <FbBasicInfo loggedIn={loggedIn} />
+                <ShareDialogExample loggedIn={loggedIn} />
                 <Button
                     title='Logout Facebook'
                     onPress={logout}
@@ -107,7 +110,7 @@ function FbBasicInfo({
                     setName(name);
                 }
             });
-    
+
             const graphRequestManager = new GraphRequestManager();
             graphRequestManager.addRequest(graphRequest).start();
         });
@@ -142,6 +145,111 @@ function FbBasicInfo({
     )
 }
 
+function ShareDialogExample({
+    loggedIn
+}) {
+    const [contentDescription, setContentDescription] = useState('');
+    const [contentUrl, setContentUrl] = useState('https://facebook.com');
+
+    if (loggedIn === false)
+        return null;
+
+    useEffect(() => {
+        AccessToken.getCurrentAccessToken()
+            .then(data => {
+                if (data === null)
+                    return;
+                const { accessToken, userID } = data;
+                console.log(data);
+                fetch(`https://graph.facebook.com/${userID}/accounts?access_token=${accessToken}`)
+                .then(response => response.json())
+                .then(json => console.log(json));
+            })
+    }, []);
+
+    function shareLink() {
+        const shareLinkContent: ShareLinkContent = {
+            contentType: 'link',
+            contentUrl,
+            contentDescription,
+            contentTitle: 'Facebook',
+            commonParameters: {
+                hashtag: '#abc',
+                peopleIds: ['100043395240724']
+            }
+        };
+
+        ShareDialog.canShow(shareLinkContent)
+            .then((canShow) => {
+                if (canShow) {
+                    return ShareDialog.show(shareLinkContent);
+                }
+            }).then((result) => {
+                console.log(result);
+                if (result.isCancelled) {
+                    console.log('Share cancelled');
+                } else {
+                    console.log('Share success with postId: '
+                        + result.postId);
+                }
+            })
+    }
+
+    function sharePhoto() {
+        const sharePhotoContent: SharePhotoContent = {
+            contentType: 'photo',
+            photos: [{
+                imageUrl: 'https://i.ytimg.com/vi/T7jFKia58M8/maxresdefault.jpg',
+                caption: 'Hihi'
+            }],
+            commonParameters: {
+                peopleIds: ['100043395240724']
+            }
+        }
+
+        ShareDialog.canShow(sharePhotoContent)
+            .then((canShow) => {
+                if (canShow) {
+                    return ShareDialog.show(sharePhotoContent);
+                }
+            }).then((result) => {
+                console.log(result);
+                if (result.isCancelled) {
+                    console.log('Share cancelled');
+                } else {
+                    console.log('Share success with postId: '
+                        + result.postId);
+                }
+            })
+    }
+
+    return (
+        <View style={styles.sharingContainer}>
+            <TextInput
+                value={contentUrl}
+                placeholder='Content Url'
+                onChangeText={(text) => setContentUrl(text)}
+            />
+            <TextInput
+                value={contentDescription}
+                placeholder='Content Description'
+                onChangeText={(text) => setContentDescription(text)}
+            />
+            <Button
+                style={styles.sharingButton}
+                title='Share'
+                onPress={shareLink}
+                disabled={contentUrl === '' || contentDescription === ''}
+            />
+            <Button
+                style={styles.sharingButton}
+                title='Share'
+                onPress={sharePhoto}
+            />
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -155,7 +263,10 @@ const styles = StyleSheet.create({
     },
     name: {
         marginBottom: 10,
-    }
+    },
+    sharingContainer: {
+        marginVertical: 10,
+    },
 });
 
 export default App;
